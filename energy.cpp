9335265e852bb3a9c51bde80b47c9f9f2b5a921c
Enrique Rodríguez <oenriquerdzlpz@gmail.com>
2023-04-18 13:09:12 -0600

    add counters

--- energy.cpp
+++ energy.cpp
@@ -52,6 +52,8 @@ double ldg_energy(double* Qold) {
 	double trace2 = 0.;
 	double trace3 = 0.;
 	int contador = 0;
+	int counter_in = 0;
+	int counter_out = 0;
 
 	for (int i = 0; i < droplet; i++) {
 		if (signal[i] == 0 || signal[i] == 1) {
@@ -68,16 +70,19 @@ double ldg_energy(double* Qold) {
 
 			if (h_bulktype[i] == 1) {
 				ldg_ans += 0.5 * (1. - U / 3.) * trace2 - U / 3. * trace3 + U * 0.25 * trace2 * trace2;
+				counter_in++;
 			}
 			else if (h_bulktype[i] == 2) {
 				ldg_ans += 0.5 * (1. - U2 / 3.) * trace2 - U2 / 3. * trace3 + U2 * 0.25 * trace2 * trace2;
+				counter_out++;
 			}
 			contador++;
 		}
 	}
 
 	printf("El contador es igual a %d\n", contador);
-
+	printf("El contador in es igual a %d\n", counter_in);
+	printf("El contador es out igual a %d\n", counter_out);
 	return dV * ldg_ans;
 }
 
